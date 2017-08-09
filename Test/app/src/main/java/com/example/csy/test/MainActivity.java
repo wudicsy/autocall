804e9cf40785f29f238af20e4e182e5964ddadeb
csy无敌 <373635314@qq.com>
2017-08-09 19:07:01 +0800

    test1 add test1

--- Test/app/src/main/java/com/example/csy/test/MainActivity.java
+++ Test/app/src/main/java/com/example/csy/test/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity
 {
     String a = "1";
+    String b = "test1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
